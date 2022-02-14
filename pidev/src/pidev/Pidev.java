7316abe3606cf563721ac79d9e80e343be95a115
SalemZacharia <99037612+SalemZacharia@users.noreply.github.com>
2022-02-14 13:29:27 +0100

    commande menucommande

--- pidev/src/pidev/Pidev.java
+++ pidev/src/pidev/Pidev.java
@@ -6,9 +6,11 @@
 package pidev;
 import java.sql.Date;
 import entities.Commande;
+import entities.MenuCommande;
 import java.time.LocalDateTime;
 import services.CommandeService;
 import utils.DataSource;
+import services.MenuCommandeService;
 
 /**
  *
@@ -25,13 +27,23 @@ public class Pidev {
         Date currentDate=new Date(2021-07-01);
         Commande c=new Commande(1,"en livraison",currentDate,1);
         Commande c1=new Commande(2,"en attente",currentDate,1);
-        System.out.println(c);
+        /*System.out.println(c); */
         CommandeService cs=new CommandeService();
         /*cs.ajouterCommande(c1);*/
-        Commande c2= new Commande(3,"En livraison",currentDate,1);
+       /* Commande c2= new Commande(3,"En livraison",currentDate,1);
         Commande c3= new Commande(3);
         cs.supCommande(c3);
-        cs.modifiercommande(c2);
+        cs.modifiercommande(c2); */
+       
+       MenuCommande mc=new MenuCommande(1,currentDate,1,1);
+       MenuCommande mc1=new MenuCommande(2,currentDate,4,1);
+      MenuCommandeService mcs=new MenuCommandeService();
+       /*mcs.ajouterMenuCommande(mc); */
+       /*mcs.modifierMenucommande(mc1); */
+       System.out.println(mcs.afficherMenuCommande());
+       System.out.println(cs.afficherCommande());
+       
+      
        
         
     }
